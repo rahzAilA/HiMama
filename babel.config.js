dd53d4d08c8289c4489bbb89da9e0eb810cd40f7
rahzAilA <umarazharthegreat5@gmail.com>
2021-12-31 01:02:43 +0500

    setup module  and core imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "@babel/plugin-transform-react-jsx",
+        {
+          runtime: "automatic",
+        },
+      ],
+      [
+        require.resolve("babel-plugin-module-resolver"),
+        {
+          extensions: [".ios.tsx?", ".android.tsx?", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "~": "./src",
+            "@modules/classrooms": "./modules/classrooms/public",
+          },
+        },
+      ],
+    ],
+  };
 };
